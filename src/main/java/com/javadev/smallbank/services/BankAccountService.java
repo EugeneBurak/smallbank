9d266eef6e2e7bba35400ef5c1ebb65d34e10c62
Evgeniy <bea@sgsoft.com.ua>
2017-09-04 12:14:04 +0300

    html run!!!

--- src/main/java/com/javadev/smallbank/services/BankAccountService.java
+++ src/main/java/com/javadev/smallbank/services/BankAccountService.java
@@ -4,9 +4,6 @@ import com.javadev.smallbank.entity.BankAccount;
 
 import java.util.List;
 
-/**
- * Created by Женя on 30.08.2017.
- */
 public interface BankAccountService {
     BankAccount addBankAccount(BankAccount bankAccount);
     void deleteBankAccount(int id);
